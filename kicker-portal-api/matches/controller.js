22354b44a6cde45df1028694338453d7830bf596
Juan Vizcaino <elvizcacho@gmail.com>
2018-04-26 08:11:42 +0200

    Add match add service

--- kicker-portal-api/matches/controller.js
+++ kicker-portal-api/matches/controller.js
@@ -1,6 +1,8 @@
 const Match = require('./model');
 const User = require('../users').model;
 const Team = require('../teams').model;
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
 
 function list(req, res) {
   Match
@@ -54,4 +56,108 @@ function list(req, res) {
   
 }
 
+function add(req, res) {
+  
+  const Team1User1Id = req.body.team1.user1Id;
+  const Team1User2Id = req.body.team1.user2Id;
+  
+  const Team2User1Id = req.body.team2.user1Id;
+  const Team2User2Id = req.body.team2.user2Id;
+  
+  const score1 = req.body.score.team1;
+  const score2 = req.body.score.team2;
+  
+  const team1Query = Team.findOrCreate({
+    where: {
+      [Op.and]: {
+        user1Id: [Team1User1Id, Team1User2Id],
+        user2Id: [Team1User1Id, Team1User2Id]
+      }
+    },
+    defaults: {
+      user1Id: Team1User1Id,
+      user2Id: Team1User2Id
+    }
+  });
+  
+  const team2Query = Team.findOrCreate({
+    where: {
+      [Op.and]: {
+        user1Id: [Team2User1Id, Team2User2Id],
+        user2Id: [Team2User1Id, Team2User2Id]
+      }
+    },
+    defaults: {
+      user1Id: Team2User1Id,
+      user2Id: Team2User2Id
+    }
+  });
+  
+  Promise.all([team1Query, team2Query]).then(teams => {
+    const team1 = teams[0][0];
+    const team2 = teams[1][0];
+    return Match.create({
+      matchOwnerId: req.user.id,
+      team1Id: team1.id,
+      team2Id: team2.id,
+      score1: score1,
+      score2: score2
+    });
+    
+  })
+  .then(match => {
+    return Match
+      .findOne({
+        where: {
+          id: match.id
+        },
+        attributes: ['id', 'score1', 'score2', 'createdAt', 'updatedAt'],
+        include: [ 
+          { 
+            model: User, 
+            as: 'matchOwner',
+            attributes: ['id', 'firstName', 'lastName']
+          },
+          {
+            model: Team, 
+            as: 'team1',
+            attributes: ['id'],
+            include: [
+              { 
+                model: User, 
+                as: 'user1',
+                attributes: ['id', 'firstName', 'lastName']
+              },
+              { 
+                model: User, 
+                as: 'user2',
+                attributes: ['id', 'firstName', 'lastName']
+              }
+            ]
+          },
+          {
+            model: Team, 
+            as: 'team2',
+            attributes: ['id'],
+            include: [
+              { 
+                model: User, 
+                as: 'user1',
+                attributes: ['id', 'firstName', 'lastName']
+              },
+              { 
+                model: User, 
+                as: 'user2',
+                attributes: ['id', 'firstName', 'lastName']
+              }
+            ]
+          }
+        ]
+      });
+  })
+  .then(match => res.send(match))
+  .catch(e => res.send(e));
+}
+
 module.exports.list = list;
+module.exports.add = add;
--- kicker-portal-api/matches/routes.js
+++ kicker-portal-api/matches/routes.js
@@ -1,6 +1,8 @@
+const bodyParser = require('body-parser');
 const controller = require('./controller');
 const passport = require('passport');
 
 module.exports = (app) => {
   app.get('/matches', passport.authenticate('jwt', {session: false}), (req, res) => controller.list(req, res));
+  app.post('/matches', passport.authenticate('jwt', {session: false}), bodyParser.json(), (req, res) => controller.add(req, res));
 }
