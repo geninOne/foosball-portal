6cae2bd69d192d0faebae68c7a0241239489c893
Juan Vizcaino <elvizcacho@gmail.com>
2018-06-20 15:01:42 +0200

    Allow cross origin

--- kicker-portal-api/app.js
+++ kicker-portal-api/app.js
@@ -8,6 +8,21 @@ require('./associations');
 // load authentication
 require('./authentication');
 
+
+//Allow api cross domain
+app.use(function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+	res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,HEAD,DELETE,OPTIONS');
+	res.header("Access-Control-Allow-Headers", req.headers['access-control-request-headers']);
+	res.header('Access-Control-Max-Age', 60 * 60 * 24 * 365);
+	// intercept OPTIONS method
+	if (req.method === 'OPTIONS') {
+		res.sendStatus(200);
+	} else {
+		next();
+	}
+});
+  
 // load routes
 require('./routes')(app);
 
